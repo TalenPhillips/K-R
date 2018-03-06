6ab8ff0a46966acbb0a8746ce0a12f5f66d39104
Talen Phillips <Talen.Phillips@mnsu.edu>
2018-03-06 01:58:04 -0600

    Added comments

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,19 @@
+/* ┌─────────────┬─────────────────────────────────────────────────────────────┐
+ * │    Title    │ "Hello World"                                               │
+ * ├─────────────┼─────────────────────────────────────────────────────────────┤
+ * │   Author    │ Talen Phillips                                              │
+ * ├─────────────┼─────────────────────────────────────────────────────────────┤
+ * │    Date     │ 06MAR2018                                                   │
+ * ├─────────────┼─────────────────────────────────────────────────────────────┤
+ * │   Program   │ This program simply prints the text "Hello World" to the    │
+ * │ Description │ command line.                                               │
+ * └─────────────┴─────────────────────────────────────────────────────────────┘
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
-    puts("Hello, world!");
+    puts("Hello, world!");  // puts() is used for simplicity and performance.
     return 0;
 }
