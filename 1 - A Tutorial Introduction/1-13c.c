5805576e587fdfa9eb431c291984ea59163ae3e9
Talen Phillips <Talen.Phillips@mnsu.edu>
2018-12-08 19:39:16 -0600

    Refactored 1-13c.c

--- 1 - A Tutorial Introduction/1-13c.c
+++ 1 - A Tutorial Introduction/1-13c.c
@@ -1,5 +1,5 @@
-/* "TXT Analyser" by Talen Phillips
- * written on 23MAY2013
+/* "Text Analyser" by Talen Phillips
+ * written 23MAY2013 - 08DEC2018
  * This program analyses a file of ASCII text, and prints out stats and graphs.
  * It was originally written as an exercise (K&R2: 1-13 and 1-14), but became an
  * exploratory project. */
@@ -11,6 +11,8 @@
 #define IN 1
 #define OUT 0
 #define CPY(to, from)   for (int cpycntr = 0; (to[cpycntr] = from[cpycntr]) != '\0'; ++cpycntr);
+
+
 
 typedef struct sstrct
 {
@@ -26,77 +28,72 @@ typedef struct sstrct
 } stat_struct;
 
 
-stat_struct init_struct(stat_struct statistics)
+
+/* Function:    Open File
+ *
+ * Description: Opens a file to be read. Displays a message if an error occurs.
+ *
+ * Arguments:   none
+ *
+ * Returns:     a pointer to the file
+ */
+FILE *open_file()
 {
-    statistics.file_ptr = open_file();
-    statistics.chars = 0;
-    statistics.letters = 0;
-    statistics.words = 0;
-    statistics.sentences = 0;
+    char c, filename[200]= {0};
+    int i;
+    FILE *file_ptr = NULL;
 
-    for(int i = 0; i < 46; i++)
+    printf("Enter a file name: ");
+    for (i = 0; (c = getchar()) != '\n' && i < 200; ++i)
     {
-        statistics.long_word[i] = 0;
+        filename[i] = c;
     }
+    putchar('\n');
 
-    for(int i = 0; i < MAXLINE; i++)
-    {
-        statistics.long_sen[i] = 0;
-    }
+    file_ptr = fopen(filename,"r");
 
-    for(int i = 0; i < MAX_LENGTH; i++)
-    {
-        statistics.word_lengths[i] = 0;
-    }
-
-    for(int i = 0; i < 26; i++)
-    {
-        statistics.letter_frequency[i] = 0;
-    }
-
-    return statistics;
+    return file_ptr;
 }
 
 
 
-/* Function:    Longest sentence
+/* Function:    Initialize structure
  *
- * Description: Looks at each sentence, determines its length, stores it in the
- *              line array from the stats function, and returns it's length.
+ * Description: Initializes the stat_struct pointed to by the argument.
  *
- * Arguments:   File pointer: the file being manipulated.
- *              s[]: The sentence in question
- *              lim: Max sentence length
+ * Arguments:   statistics: pointer to a stat_struct
  *
- * Returns:     The length of the sentence.
+ * Returns:     nothing
  */
-int longest_sen(FILE *file_ptr, char s[], int lim)
+void init_struct(stat_struct * statistics)
 {
-    int c = '.', i;
+    statistics->file_ptr = open_file();
+    statistics->chars = 0;
+    statistics->letters = 0;
+    statistics->words = 0;
+    statistics->sentences = 0;
 
-    for (i = 0;
-        i < lim-1 &&
-        (c = fgetc(file_ptr)) != EOF &&
-        c != '.' &&
-        c != '_';
-        ++i)
+    for(int i = 0; i < 46; i++)
     {
-        s[i] = c;
+        statistics->long_word[i] = 0;
     }
 
-    if (c == '\n')
+    for(int i = 0; i < MAXLINE; i++)
     {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-
-    if (i == 0 && c != EOF)
-    {
-        i = 1;
+        statistics->long_sen[i] = 0;
     }
 
-    return i;
+    for(int i = 0; i < MAX_LENGTH; i++)
+    {
+        statistics->word_lengths[i] = 0;
+    }
+
+    for(int i = 0; i < 26; i++)
+    {
+        statistics->letter_frequency[i] = 0;
+    }
+
+    return;
 }
 
 
@@ -113,21 +110,21 @@ int longest_sen(FILE *file_ptr, char s[], int lim)
  */
 void print_table(int arr[], int arr_len)
 {
-	int big = 0, division = 1, rounded = 1;
+    int big = 0, division = 1, rounded = 1;
 
-	/* find the largest element. */
-	for (int i = 0; i < arr_len; ++i)
+    /* find the largest element. */
+    for (int i = 0; i < arr_len; ++i)
     {
-		if (arr[i] > big)
+        if (arr[i] > big)
         {
-			big = arr[i];
+            big = arr[i];
         }
     }
 
-	/* Find a divisor */
-	if (big > MAX_HEIGHT)
+    /* Find a divisor */
+    if (big > MAX_HEIGHT)
     {
-		division = big/MAX_HEIGHT;
+        division = big/MAX_HEIGHT;
     }
     /* round the divisor */
     for (int i = 1; rounded < division; rounded += i)
@@ -144,14 +141,14 @@ void print_table(int arr[], int arr_len)
     {
         rounded += division;
     }
-	big = rounded / division;
+    big = rounded / division;
 
-	/* Print the vertical axis and the bars */
-	for (; big > 0; --big)
+    /* Print the vertical axis and the bars */
+    for (; big > 0; --big)
     {
-		printf("%6d │", big * division);
+        printf("%6d │", big * division);
 
-		for (int i = 0; i < arr_len; ++i)
+        for (int i = 0; i < arr_len; ++i)
         {
             rounded = ((arr[i] - ((big - 1) * division)) * 1000) / division;
 
@@ -195,48 +192,20 @@ void print_table(int arr[], int arr_len)
             {
                 printf(" █");
             }
-		}
+        }
 
-		putchar('\n');
-	}
-
-	/* Print horizontal axis (line only) */
-	printf("───────┼");
-
-	for (int i = 0; i < arr_len; ++i)
-    {
-		printf("──");
+        putchar('\n');
     }
 
-	return;
-}
+    /* Print horizontal axis (line only) */
+    printf("───────┼");
 
-
-
-/* Function:    Open File
- *
- * Description: Opens a file to be read. Displays a message if an error occurs.
- *
- * Arguments:   none
- *
- * Returns:     a pointer to the file
- */
-FILE *open_file()
-{
-	char c, filename[200]= {0};
-	int i;
-	FILE *file_ptr = NULL;
-
-	printf("Enter a file name: ");
-	for (i = 0; (c = getchar()) != '\n' && i < 200; ++i)
+    for (int i = 0; i < arr_len; ++i)
     {
-		filename[i] = c;
+        printf("──");
     }
-	putchar('\n');
 
-	file_ptr = fopen(filename,"r");
-
-	return file_ptr;
+    return;
 }
 
 
@@ -255,38 +224,63 @@ FILE *open_file()
  *
  * Returns:     nothing
  */
-stat_struct analyze(stat_struct statistics)
+void analyze(stat_struct * statistics)
 {
-	char c, state = OUT;
-    char currentWord[46] = { 0 };
-	int word_len = 0, max_word = 0;
+    char c, state = OUT;
+    char currentWord[46] = { 0 }, currentSentence[MAXLINE] = { 0 };
+    int word_len = 0, sen_len = 0, max_word = 0, max_sen = 0;
 
-	/* Count each word and increment the corresponding array element.
-	 * This loop also contains char, letter, word, and sentence counters.*/
-	while ((c = fgetc(statistics.file_ptr)) != EOF)
+    /* Count each word and increment the corresponding array element.
+     * This loop also contains char, letter, word, and sentence counters.*/
+    while ((c = fgetc(statistics->file_ptr)))
     {
-		if (c == '.')
+        if (c == '_' || c == '.' || c == EOF)
         {
-			++statistics.sentences; /* (need better definition of sentence) */
+            if (sen_len >= 10)
+            {
+                ++statistics->sentences; /* need better definition of sentence */
+                if (sen_len > max_sen)
+                {
+                    max_sen = sen_len;
+                    sen_len = 0;
+                    CPY(statistics->long_sen, currentSentence);
+                    statistics->long_sen[max_sen] = c;
+                }
+            }
+
+            for (int i = 0; i <= sen_len; i++)
+            {
+                currentSentence[i] = '\0';
+            }
+            sen_len = 0;
+
+            if (c == EOF)
+            {
+                break;
+            }
         }
-		else if (c == ' ' || c == '\t' || c == '\n')
+        else if (sen_len < MAXLINE-1 && ((c >= ' ' && c <= '~') || c == '\n'))
         {
-			state = OUT;
+            currentSentence[sen_len++] = c;
+        }
 
-			if (word_len >= MAX_LENGTH)
-            {
-                ++statistics.word_lengths[MAX_LENGTH-1];
-            }
-			else
-            {
-                ++statistics.word_lengths[word_len];
-            }
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
+            state = OUT;
 
+            if (word_len >= MAX_LENGTH)
+            {
+                ++statistics->word_lengths[MAX_LENGTH-1];
+            }
+            else
+            {
+                ++statistics->word_lengths[word_len];
+            }
 
             if (word_len > max_word)
             {
                 max_word = word_len;
-                CPY(statistics.long_word, currentWord);
+                CPY(statistics->long_word, currentWord);
             }
 
             for (int i = 0; i < 46; i++)
@@ -294,51 +288,38 @@ stat_struct analyze(stat_struct statistics)
                 currentWord[i] = 0;
             }
 
-			word_len = 0;
-		}
-		else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            word_len = 0;
+        }
+        else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
         {
-			if (c >= 'a' && c <= 'z')
+            if (c >= 'a' && c <= 'z')
             {
-				++statistics.letter_frequency[c-97];
+                ++statistics->letter_frequency[c-97];
             }
-			else
+            else
             {
-				++statistics.letter_frequency[c-65];
+                ++statistics->letter_frequency[c-65];
             }
 
-			if (state == OUT)
+            if (state == OUT)
             {
-				state = IN;
-				++statistics.words;
-			}
+                state = IN;
+                ++statistics->words;
+            }
 
-		    ++statistics.letters;
-		    currentWord[word_len++] = c;
-		}
+            ++statistics->letters;
+            currentWord[word_len++] = c;
+        }
         else if (c == '\'' || (c >= '0' && c <= '9'))
         {
-		    ++statistics.letters;
-		    currentWord[word_len++] = c;
+            ++statistics->letters;
+            currentWord[word_len++] = c;
         }
-	    ++statistics.chars;
-	}
 
-    rewind(statistics.file_ptr);
-
-
-    /* Find the longest sentence and print it. */
-    max = 0;
-    while ((statistics. = longest_sen(statistics.file_ptr, line, MAXLINE)) > 0)
-    {
-        if (len > max)
-        {
-            max = len;
-            CPY(statistics.long_sen, line);
-        }
+        ++statistics->chars;
     }
 
-	return;
+    return;
 }
 
 
@@ -352,70 +333,68 @@ stat_struct analyze(stat_struct statistics)
  *
  * Returns:     nothing
  */
-void display(stat_struct statistics)
+void display(stat_struct * statistics)
 {
-	/***** Table 1: Word Length Histogram *****/
-	printf(" Table:     Word Lengths\n───────┬────────────────\n");
-	print_table(statistics.word_lengths+1, MAX_LENGTH-1);
+    /***** Table 1: Word Length Histogram *****/
+    printf(" Table:     Word Lengths\n───────┬────────────────\n");
+    print_table(statistics->word_lengths+1, MAX_LENGTH-1);
 
     /* Print horizontal axis legend: */
-	printf("\n       │");
-	for (i = 1; i < (MAX_LENGTH-1); ++i)
+    printf("\n       │");
+    for (int i = 1; i < (MAX_LENGTH-1); ++i)
     {
-		if (!(i % 5))
+        if (!(i % 5))
         {
-			printf("%2d", i);
+            printf("%2d", i);
         }
-		else
+        else
         {
-			printf("  ");
+            printf("  ");
         }
-	}
-	printf(" +\n\n");
-
-
-	/***** Table 2: Letter Frequency Histogram *****/
-	printf(" Table:   Letter Frequency\n───────┬──────────────────\n");
-	print_table(statistics.letter_frequency, 26);
-
-	/* Print horizontal axis legend: */
-	printf("\n       │");
-	for (i = 0; i < 26; ++i)
-    {
-		printf(" %c", i + 65);
     }
-	printf("\n\n");
+    printf(" +\n\n");
 
-    /* Re-initialize file pointer */
-	//file_ptr = fopen(filename,"r");
-	//if (file_ptr == NULL)
-    //{
-    //    perror("Error opening file");
-    //    return;
-	//}
 
-	/****************** Print totals and averages ******************/
-	printf("Number of characters: %d\n",statistics.chars);
-	printf("Number of letters:    %d\n",statistics.letters);
-	printf("Number of words:      %d\n",num_word);
-	printf("Number of sentences:  %d\n\n",statistics.sentences);
-	printf("Mean word length:     %.2f letters\n",1.0*statistics.letters/num_word);
-	printf("Mean sentence length: %.2f words\n\n",1.0*num_word/statistics.sentences);
+    /***** Table 2: Letter Frequency Histogram *****/
+    printf(" Table:   Letter Frequency\n───────┬──────────────────\n");
+    print_table(statistics->letter_frequency, 26);
 
-    printf("Longest word (%i characters): %s\n\n", max_word, statistics.long_word);
-
-    if (max > 0)
+    /* Print horizontal axis legend: */
+    printf("\n       │");
+    for (int i = 0; i < 26; ++i)
     {
-        printf("\nLongest sentence: %d characters.\n%s. \n\n", max, longest);
+        printf(" %c", i + 65);
+    }
+    printf("\n\n");
+
+
+    int max_word = 0;
+    while (statistics->long_word[max_word++] != '\0');
+
+    int max_sen = 0;
+    while (statistics->long_sen[max_sen++] != '\0');
+
+    /****************** Print totals and averages ******************/
+    printf("Number of characters: %d\n", statistics->chars);
+    printf("Number of letters:    %d\n", statistics->letters);
+    printf("Number of words:      %d\n", statistics->words);
+    printf("Number of sentences:  %d\n\n", statistics->sentences);
+    printf("Mean word length:     %.2f letters\n",1.0*statistics->letters/statistics->words);
+    printf("Mean sentence length: %.2f words\n\n",1.0*statistics->words/statistics->sentences);
+
+    printf("Longest word (%i characters): %s\n\n", max_word, statistics->long_word);
+
+    if (max_sen > 0)
+    {
+        printf("\nLongest sentence: %d characters.\n%s. \n\n", max_sen, statistics->long_sen);
     }
     else
     {
         printf("The longest sentence is: 0 \n\n");
     }
 
-    fclose(file_ptr);
 
-	return;
+    return;
 }
 
 
@@ -423,31 +402,32 @@ void display(stat_struct statistics)
 /* * * * * * * * * * * * * * * * * * * MAIN * * * * * * * * * * * * * * * * * */
 int main(void)
 {
-	char cont = 'N', c;
+    char cont = 'N', c;
     stat_struct statistics;
 
-	do {
-        statistics = init_struct(statistics);
+    do {
+        init_struct(&statistics);
 
-        if (file_ptr != NULL)
+        if (statistics.file_ptr != NULL)
         {
-	        statistics = analyze(statistics);
-            display(statistics);
+            analyze(&statistics);
+            display(&statistics);
+            fclose(statistics.file_ptr);
         }
         else
         {
             puts("Error opening file");
         }
 
-		printf("\nContinue? Y/N : ");
+        printf("\nContinue? Y/N : ");
 
-		while ((c = getchar()) != '\n' && c != EOF)
+        while ((c = getchar()) != '\n' && c != EOF)
         {
-			cont = c;
+            cont = c;
         }
 
-		putchar('\n');
-	} while (cont == 'Y' || cont == 'y');
+        putchar('\n');
+    } while (cont == 'Y' || cont == 'y');
 
-	return 0;
+    return 0;
 }
